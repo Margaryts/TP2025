35e2a909fcfa5a26ff453cd29312f8dbc2c1d388
Пилипчук Маргарита Сергіївна <ipz233_pms@student.ztu.edu.ua>
2025-01-30 23:22:05 +0200

    app

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SumCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int sum = a + b;
+
+            Console.WriteLine($"Sum numbers {a} and {b} =: {sum}");
+        }
+    }
+}
